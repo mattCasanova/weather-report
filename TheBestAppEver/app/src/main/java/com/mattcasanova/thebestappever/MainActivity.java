66ca48ee7d97e72e3f54e6f2b1e03efd56bf3da0
Matt Casanova <lazersquad@gmail.com>
2019-03-17 02:07:52 -0700

    Added BestAppEver

---
+++ TheBestAppEver/app/src/main/java/com/mattcasanova/thebestappever/MainActivity.java
@@ -0,0 +1,23 @@
+ package com.mattcasanova.thebestappever;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ajskldfjkla;sdjf
+
+                jaklsdjfkljalksdf
+
+                ajsdklfjkajsdlfjasd
+                        ajsdklfjkajsdlfjasd
+                        ajskdlfjklajsdkljfkajsdklfj
+
+                                jhaklsdjfkjasd'fjasdf'
+    }
+}
